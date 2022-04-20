862a89c27a94bc5277307ffe3a62b09acaeb8449
causehhc <hhc404404@gmail.com>
2022-04-20 07:10:10 -0700

    final ok

--- src/my_gui/scripts/algorithm/slam/hector/__init__.py
+++ src/my_gui/scripts/algorithm/slam/hector/__init__.py
@@ -3,7 +3,7 @@ import roslaunch
 
 class Hetor_SLAM:
     def __init__(self):
-        self.launch_file = "/home/hhc/Desktop/ros/mecanum_agv/src/my_gui/launch/hector_mapping.launch"
+        self.launch_file = "/home/hhc/Desktop/ros/mecanum_agv/src/my_hector/launch/hector_mapping.launch"
 
     def start(self):
         uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
