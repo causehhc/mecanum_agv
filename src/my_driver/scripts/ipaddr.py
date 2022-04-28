9c5431ac0dc0df3609e48243855b76cdaf064491
causehhc <hhc404404@gmail.com>
2022-04-28 03:38:31 -0700

    update ip

--- src/my_driver/scripts/ipaddr.py
+++ src/my_driver/scripts/ipaddr.py
@@ -4,10 +4,6 @@ import socket
 import time
 
 def get_host_ip():
-    """
-    查询本机ip地址
-    :return: ip
-    """
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('8.8.8.8', 80))
@@ -22,7 +18,6 @@ if __name__=='__main__':
     while True:
         ip = get_host_ip()
         cmd_str = "{}{}{}".format('"', ip, '"')
-        cmd = list(cmd_str)
-        # print(cmd)
+        cmd = bytes(cmd_str, encoding = "utf8")
         ser.write(cmd)
         time.sleep(10)
